f0c173c8f0960d00810915173f4c49e0ccc7d675
lukadeSwati <lukadeswati24@gmail.com>
2024-03-08 23:50:57 +0530

    test commited

---
+++ examplesecurity-1/src/main/java/com/springboot/web/Examplesecurity1Application.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examplesecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examplesecurity1Application.class, args);
+	}
+
+}
